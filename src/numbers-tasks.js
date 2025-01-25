4163abb9aacacbb4ca1de971fc662af193971002
Stanley Rocks <25880632+stanrocks@users.noreply.github.com>
2025-01-26 03:22:07 +0500

    feat: implement calculations of number from value, cube of number, fibonacci number by index, sum of numbers from 1 to n

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -225,8 +225,8 @@ function isPrime(n) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  return Number(value) || def;
 }
 
 /**
@@ -240,8 +240,8 @@ function toNumber(/* value, def */) {
  *   -2 => -8
  *   0  => 0
  */
-function getCube(/* num */) {
-  throw new Error('Not implemented');
+function getCube(num) {
+  return num ** 3;
 }
 
 /**
@@ -257,8 +257,12 @@ function getCube(/* num */) {
  *   3  => 2
  *   10 => 55
  */
-function getFibonacciNumber(/* index */) {
-  throw new Error('Not implemented');
+function getFibonacciNumber(index) {
+  const fibSequence = [0, 1];
+  for (let i = 2; i <= index; i += 1) {
+    fibSequence.push(fibSequence[i - 1] + fibSequence[i - 2]);
+  }
+  return fibSequence[index];
 }
 
 /**
@@ -272,8 +276,12 @@ function getFibonacciNumber(/* index */) {
  *   10 => 55 // (1+2+3+...+10)
  *   1  => 1
  */
-function getSumToN(/* n */) {
-  throw new Error('Not implemented');
+function getSumToN(n) {
+  let sum = 0;
+  for (let i = 1; i <= n; i += 1) {
+    sum += i;
+  }
+  return sum;
 }
 
 /**
